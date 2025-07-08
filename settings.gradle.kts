30a5c7ac48e928b39f7e18d72f0fea53af950a1b
zhuhk2 <zhuhk2@lenovo.com>
2025-07-08 18:11:28 +0800

    add maven mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,13 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/public/")
+
+        }
+        maven {
+            url = uri("https://mirrors.tuna.tsinghua.edu.cn/")
+        }
         google()
         mavenCentral()
     }
